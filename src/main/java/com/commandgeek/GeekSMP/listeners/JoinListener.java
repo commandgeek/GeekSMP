0050eb12fa08112134cac0bab470bd592964b5e9
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-01 15:54:37 +0100

    Escaped markdown in join message

--- src/main/java/com/commandgeek/GeekSMP/listeners/JoinListener.java
+++ src/main/java/com/commandgeek/GeekSMP/listeners/JoinListener.java
@@ -52,6 +52,7 @@ public class JoinListener implements Listener {
         event.setJoinMessage(new MessageManager("join").replace("%player%", player.getName()).string());
         new MessageManager("smp-chat-join")
                 .replace("%player%", player.getName())
+                .escapeMarkdown()
                 .sendDiscord(DiscordManager.smpChatChannel);
     }
 
