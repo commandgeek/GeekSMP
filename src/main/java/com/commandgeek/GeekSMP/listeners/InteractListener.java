98d1ab796ee733b501422369c59a2afbb3cb8771
srnyx <roweblox@gmail.com>
2022-02-06 16:12:28 -0500

    Update InteractListener.java

--- src/main/java/com/commandgeek/GeekSMP/listeners/InteractListener.java
+++ src/main/java/com/commandgeek/GeekSMP/listeners/InteractListener.java
@@ -129,13 +129,4 @@ public class InteractListener implements Listener {
             LockManager.unplace(event.getBlock());
         }
     }
-
-    @EventHandler
-    public void EntityDamageByEntityEvent (EntityDamageByEntityEvent event) {
-        if (event.getEntity() instanceof ItemFrame entity && event.getDamager() instanceof Player player) {
-            if (TeamManager.isUndead(player)) { // USING PETS: if (TeamManager.isUndead(player) && !MorphManager.isPetNearOwner(player)) {
-                event.setCancelled(true);
-            }
-        }
-    }
 }
