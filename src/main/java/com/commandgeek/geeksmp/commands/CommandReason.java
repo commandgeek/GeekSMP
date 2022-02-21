d87341a13c5ab6f4ad2468996199a948bca60adf
srnyx <25808801+srnyx@users.noreply.github.com>
2022-02-21 13:49:54 -0500

    Fix #11

--- src/main/java/com/commandgeek/geeksmp/commands/CommandReason.java
+++ src/main/java/com/commandgeek/geeksmp/commands/CommandReason.java
@@ -1,14 +1,21 @@
 package com.commandgeek.geeksmp.commands;
 
+import com.commandgeek.geeksmp.Main;
 import com.commandgeek.geeksmp.managers.*;
 
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class CommandReason implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class CommandReason implements TabExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (sender instanceof Player player && !player.hasPermission("geeksmp.command.reason") && !TeamManager.isStaff(player)) {
@@ -50,13 +57,13 @@ public class CommandReason implements CommandExecutor {
                     if (banTime == -1) {
                         new MessageManager("reason-ban-permanent")
                                 .replace("%player%", target.getName())
-                                .replace("%reason%", MuteManager.getReason(target.getUniqueId()))
+                                .replace("%reason%", BanManager.getReason(target.getUniqueId()))
                                 .send(sender);
                     } else {
                         new MessageManager("reason-ban-temporary")
                                 .replace("%player%", target.getName())
                                 .replace("%duration%", NumberManager.getTimeFrom(banTime))
-                                .replace("%reason%", MuteManager.getReason(target.getUniqueId()))
+                                .replace("%reason%", BanManager.getReason(target.getUniqueId()))
                                 .send(sender);
                     }
                 }
@@ -66,5 +73,26 @@ public class CommandReason implements CommandExecutor {
 
         new MessageManager("invalid-arguments").send(sender);
         return true;
+    }
+
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> suggestions = new ArrayList<>();
+        List<String> results = new ArrayList<>();
+        if (args.length == 1) {
+            for (String muted : Main.muted.getKeys(false)) {
+                OfflinePlayer op = Bukkit.getOfflinePlayer(UUID.fromString(muted));
+                suggestions.add(op.getName());
+            }
+            for (String banned : Main.banned.getKeys(false)) {
+                OfflinePlayer op = Bukkit.getOfflinePlayer(UUID.fromString(banned));
+                suggestions.add(op.getName());
+            }
+        }
+        for (String suggestion : suggestions) {
+            if (suggestion.toLowerCase().startsWith(args[args.length - 1].toLowerCase())) {
+                results.add(suggestion);
+            }
+        }
+        return results;
     }
 }
