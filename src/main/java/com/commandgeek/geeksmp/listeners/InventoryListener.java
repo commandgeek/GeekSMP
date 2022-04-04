b016bb2c9ecdd731e835f62c2eb3ce62818c0eae
srnyx <25808801+srnyx@users.noreply.github.com>
2022-04-04 18:51:09 -0400

    Update InventoryListener.java

--- src/main/java/com/commandgeek/geeksmp/listeners/InventoryListener.java
+++ src/main/java/com/commandgeek/geeksmp/listeners/InventoryListener.java
@@ -11,17 +11,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.UUID;
@@ -89,7 +85,7 @@ public class InventoryListener implements Listener {
             }
         }
     }
-    
+
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         Player player = event.getPlayer();
